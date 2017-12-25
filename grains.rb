eb4789c5988942485fd87e3963ef95b5e23b256e
Mark R <mirubenstein@gmail.com>
2017-12-24 21:39:02 -0800

    Update to use constants and &method

--- grains.rb
+++ grains.rb
@@ -1,11 +1,16 @@
 class Grains
+  MIN_SQUARE = 1
+  MAX_SQUARE = 64
+
   def self.square(number)
-    raise ArgumentError, "Number must be between 1 and 64. #{number} given." unless number.between?(1,64)
+    unless number.between?(MIN_SQUARE, MAX_SQUARE)
+      raise ArgumentError, "Number must be between 1 and 64. #{number} given."
+    end
     2**(number - 1)
   end
 
   def self.total
-    (1..64).sum { |square_number| square(square_number) }
+    1.upto(64).sum(&method(:square))
   end
 end
 
